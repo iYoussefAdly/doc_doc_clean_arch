4d386dc2b18f8768bfacf1e0671bdba0155270a4
Youssef <youssefadly332@gmail.com>
2026-01-25 14:24:09 +0200

    structure the app and add assets

---
+++ lib/core/utils/asset_data.dart
@@ -0,0 +1,8 @@
+abstract class AssetData {
+  final docdocImage = "assets/images/splash and on boarding images/Docdoc.png";
+  final groupImage = "assets/images/splash and on boarding images/Group.png";
+  final vector1Image =
+      "assets/images/splash and on boarding images/Vector (1).png";
+  final vector2Image =
+      "assets/images/splash and on boarding images/Vector (2).png";
+}
