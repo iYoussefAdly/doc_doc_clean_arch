43f75b4d12f3f5fc394824fb15cac1bf18f781fe
Youssef <youssefadly332@gmail.com>
2026-01-30 15:42:39 +0200

    finish domain layer of auth feature

--- lib/features/auth/domain/use_cases/register_use_case.dart
+++ lib/features/auth/domain/use_cases/register_use_case.dart
@@ -1,15 +1,15 @@
 import 'package:doc_doc_clean_arch/core/use_case/use_case.dart';
 import 'package:doc_doc_clean_arch/features/auth/domain/entities/user_entity.dart';
 import 'package:doc_doc_clean_arch/features/auth/domain/repos/auth_repo.dart';
-import 'package:doc_doc_clean_arch/features/auth/domain/use_cases/params/login_params.dart';
+import 'package:doc_doc_clean_arch/features/auth/domain/use_cases/params/register_params.dart';
 
-class LoginUseCase extends UseCase<UserEntity, LoginParams> {
+class RegisterUseCase extends UseCase<UserEntity, RegisterParams> {
   final AuthRepo authRepo;
 
-  LoginUseCase({required this.authRepo});
+  RegisterUseCase({required this.authRepo});
   @override
-  Future<UserEntity> call(LoginParams params) {
+  Future<UserEntity> call(RegisterParams params) {
     //check user permissions
-   return authRepo.login(params);
+    return authRepo.register(params);
   }
 }
