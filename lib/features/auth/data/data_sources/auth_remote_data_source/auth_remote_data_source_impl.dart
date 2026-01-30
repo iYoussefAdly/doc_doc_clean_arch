894dbc04436235f0ce9b20d3f6f51a7d1a18707b
Youssef <youssefadly332@gmail.com>
2026-01-30 17:09:59 +0200

    finish auth repo impl

--- lib/features/auth/data/data_sources/auth_remote_data_source/auth_remote_data_source_impl.dart
+++ lib/features/auth/data/data_sources/auth_remote_data_source/auth_remote_data_source_impl.dart
@@ -1,10 +1,11 @@
 import 'package:doc_doc_clean_arch/constants.dart';
 import 'package:doc_doc_clean_arch/core/utils/api_services.dart';
-import 'package:doc_doc_clean_arch/features/auth/data/models/data_sources/auth_local_data_souce/auth_local_data_source.dart';
-import 'package:doc_doc_clean_arch/features/auth/data/models/data_sources/auth_remote_data_source/auth_remote_data_source.dart';
+import 'package:doc_doc_clean_arch/features/auth/data/data_sources/auth_local_data_souce/auth_local_data_source.dart';
+import 'package:doc_doc_clean_arch/features/auth/data/data_sources/auth_remote_data_source/auth_remote_data_source.dart';
 import 'package:doc_doc_clean_arch/features/auth/data/models/user_model.dart';
 import 'package:doc_doc_clean_arch/features/auth/domain/use_cases/params/login_params.dart';
 import 'package:doc_doc_clean_arch/features/auth/domain/use_cases/params/register_params.dart';
+
 class AuthRemoteDataSourceImpl extends AuthRemoteDataSource {
   final ApiServices apiServices;
   final AuthLocalDataSource localDataSource;
@@ -17,10 +18,7 @@ class AuthRemoteDataSourceImpl extends AuthRemoteDataSource {
     try {
       final response = await apiServices.post(
         endPoint: kLoginEndPoint,
-        data: {
-          'email': params.email,
-          'password': params.password,
-        },
+        data: {'email': params.email, 'password': params.password},
       );
       final token = response['data']['token'];
       if (token != null) {
@@ -40,9 +38,9 @@ class AuthRemoteDataSourceImpl extends AuthRemoteDataSource {
           'email': params.email,
           'password': params.password,
           'password_confirmation': params.confirmPassword,
-          if (params.name != null) 'name': params.name,
-          if (params.phone != null) 'phone': params.phone,
-          if (params.gender != null) 'gender': params.gender,
+          'name': params.name,
+          'phone': params.phone,
+          'gender': params.gender,
         },
       );
       final token = response['data']['token'];
---
+++ lib/features/auth/data/repos/auth_repo_impl.dart
@@ -0,0 +1,34 @@
+import '../../domain/entities/user_entity.dart';
+import '../../domain/repos/auth_repo.dart';
+import '../data_sources/auth_local_data_souce/auth_local_data_source.dart';
+import '../data_sources/auth_remote_data_source/auth_remote_data_source.dart';
+import '../models/user_model.dart';
+import '../../domain/use_cases/params/login_params.dart';
+import '../../domain/use_cases/params/register_params.dart';
+
+class AuthRepoImpl implements AuthRepo {
+  final AuthRemoteDataSource remoteDataSource;
+  final AuthLocalDataSource localDataSource;
+  AuthRepoImpl({
+    required this.remoteDataSource,
+    required this.localDataSource,
+  });
+  @override
+  Future<UserEntity> login(LoginParams params) async {
+    final UserModel userModel = await remoteDataSource.login(params);
+    return _mapToEntity(userModel);
+  }
+  @override
+  Future<UserEntity> register(RegisterParams params) async {
+    final UserModel userModel = await remoteDataSource.register(params);
+    return _mapToEntity(userModel);
+  }
+  UserEntity _mapToEntity(UserModel model) {
+    return UserEntity(
+      name: model.name,
+      email: model.email,
+      phone: model.phone,
+      gender: model.gender,
+    );
+  }
+}
