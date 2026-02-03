b9e35c12133d39558f96d3db06eb8dc0bb19cfee
Youssef <youssefadly332@gmail.com>
2026-02-03 16:16:49 +0200

    add homerepo abstract class

--- lib/features/home/domain/repos/home_repo.dart
+++ lib/features/home/domain/repos/home_repo.dart
@@ -1,3 +1,7 @@
+import 'package:dartz/dartz.dart';
+import 'package:doc_doc_clean_arch/core/errors/failure.dart';
+import 'package:doc_doc_clean_arch/features/home/domain/entities/doctor_entity.dart';
+
 abstract class HomeRepo {
-  
+  Future<Either<Failure, List<DoctorEntity>>> getDoctors();
 }
