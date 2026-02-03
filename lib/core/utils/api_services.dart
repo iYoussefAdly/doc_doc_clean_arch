4303f520f54d6c430d3323149944d99eb386201b
Youssef <youssefadly332@gmail.com>
2026-02-03 16:32:13 +0200

    add token param in get request in api services

--- lib/core/utils/api_services.dart
+++ lib/core/utils/api_services.dart
@@ -6,11 +6,15 @@ class ApiServices {
 
   static const String baseUrl = "https://vcare.integration25.com/api";
 
-  Future<Map<String, dynamic>> get({required String endPoint}) async {
-    final response = await dio.get("$baseUrl$endPoint");
-    return response.data;
-  }
-
+  Future<Map<String, dynamic>> get({required String endPoint, String? token}) async {
+  final response = await Dio().get(
+    "$baseUrl$endPoint",
+    options: Options(
+      headers: token != null ? {"Authorization": "Bearer $token"} : null,
+    ),
+  );
+  return response.data;
+}
   Future<Map<String, dynamic>> post({
     required String endPoint,
     Map<String, dynamic>? data,
