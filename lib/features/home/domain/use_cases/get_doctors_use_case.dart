96290bb44ea078f9e7a93df301077c0e3593efb6
Youssef <youssefadly332@gmail.com>
2026-02-03 16:24:05 +0200

    add GetDoctorsUseCase

---
+++ lib/features/home/domain/use_cases/get_doctors_use_case.dart
@@ -0,0 +1,15 @@
+import 'package:dartz/dartz.dart';
+import 'package:doc_doc_clean_arch/core/errors/failure.dart';
+import 'package:doc_doc_clean_arch/core/use_case/use_case.dart';
+import 'package:doc_doc_clean_arch/features/home/domain/entities/doctor_entity.dart';
+import 'package:doc_doc_clean_arch/features/home/domain/repos/home_repo.dart';
+class GetDoctorsUseCase
+    extends UseCase<Either<Failure, List<DoctorEntity>>, void> {
+  final HomeRepo homeRepo;
+  GetDoctorsUseCase({required this.homeRepo});
+  @override
+  Future<Either<Failure, List<DoctorEntity>>> call(void params) {
+    //check user permission
+    return homeRepo.getDoctors();
+  }
+}
