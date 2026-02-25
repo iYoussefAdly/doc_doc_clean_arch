f2a92835fa3979a3898900f0c7947c2ee7846e4f
Youssef <youssefadly332@gmail.com>
2026-02-25 15:30:30 +0200

    finish seach cubit

--- lib/features/search/presentation/manager/search_result_cubit/search_result_cubit.dart
+++ lib/features/search/presentation/manager/search_result_cubit/search_result_cubit.dart
@@ -1,9 +1,21 @@
 import 'package:bloc/bloc.dart';
 import 'package:doc_doc_clean_arch/features/home/domain/entities/doctor_entity.dart';
+import 'package:doc_doc_clean_arch/features/search/domain/use_cases/params/search_param.dart';
+import 'package:doc_doc_clean_arch/features/search/domain/use_cases/search_use_case.dart';
 import 'package:meta/meta.dart';
-
 part 'search_result_state.dart';
 
 class SearchResultCubit extends Cubit<SearchResultState> {
-  SearchResultCubit() : super(SearchResultInitial());
+  SearchResultCubit({required this.searchUseCase})
+    : super(SearchResultInitial());
+  final SearchUseCase searchUseCase;
+  Future<void> getDoctorResult({required SearchParam param}) async {
+    emit(SearchResultLoading());
+    final result = await searchUseCase.call(param);
+    result.fold(
+      (failure) =>
+          emit(SearchResultFailure(errorMessage: failure.errorMessage)),
+      (success) => emit(SearchResultSuccess(doctorResult: success)),
+    );
+  }
 }
