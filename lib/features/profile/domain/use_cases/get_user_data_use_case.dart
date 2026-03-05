f65f05e3b06b44f2921679545be625d21792ec26
Youssef <youssefadly332@gmail.com>
2026-03-05 20:56:43 +0200

    fix error in Get UserData UseCase

--- lib/features/profile/domain/use_cases/get_user_data_use_case.dart
+++ lib/features/profile/domain/use_cases/get_user_data_use_case.dart
@@ -1,16 +1,16 @@
 import 'package:dartz/dartz.dart';
 import 'package:doc_doc_clean_arch/core/errors/failure.dart';
 import 'package:doc_doc_clean_arch/core/use_case/use_case.dart';
-import 'package:doc_doc_clean_arch/features/auth/domain/entities/user_entity.dart';
 import 'package:doc_doc_clean_arch/features/home/domain/use_cases/get_doctors_use_case.dart';
+import 'package:doc_doc_clean_arch/features/profile/domain/entities/profile_user_entity.dart';
 import 'package:doc_doc_clean_arch/features/profile/domain/repos/profile_repo.dart';
 
-class GetUserDataUseCase extends UseCase<UserEntity, NoParam> {
+class GetUserDataUseCase extends UseCase<ProfileUserEntity, NoParam> {
   final ProfileRepo profileRepo;
 
   GetUserDataUseCase({required this.profileRepo});
   @override
-  Future<Either<Failure, UserEntity>> call(NoParam params) async {
+  Future<Either<Failure, ProfileUserEntity>> call(NoParam params) async {
     return await profileRepo.getUserData();
   }
 }
