e9d1d9410b7002a0d87ad953bc6bd60011d4227d
Youssef <youssefadly332@gmail.com>
2026-03-05 20:03:16 +0200

    add profile user entity

---
+++ lib/features/profile/domain/entities/profile_user_entity.dart
@@ -0,0 +1,6 @@
+class ProfileUserEntity {
+  final String name;
+  final String email;
+  final String phone;
+  ProfileUserEntity({required this.name, required this.email, required this.phone});
+}
